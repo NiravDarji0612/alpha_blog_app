611bbcfeb9c17037275b275e727c57eab6d3bb8c
nirav <darjinirav18@gmail.com>
2021-07-26 17:16:29 +0530

    applying validations and association concepts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/new'
   root 'pages#home'
   get '/about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: %i[:new]
 end
